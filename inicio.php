2f2dd3861f376dfffec9188fe7792718b53e909a
Victor Medina <vmedina@ucol.mx>
2017-02-18 07:14:01 -0600

    Base de boostrap

---
+++ inicio.php
@@ -0,0 +1,104 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Bootstrap Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <style>
+    /* Remove the navbar's default margin-bottom and rounded borders */ 
+    .navbar {
+      margin-bottom: 0;
+      border-radius: 0;
+    }
+    
+    /* Add a gray background color and some padding to the footer */
+    footer {
+      background-color: #f2f2f2;
+      padding: 25px;
+    }
+    
+  .carousel-inner img {
+      width: 100%; /* Set width to 100% */
+      margin: auto;
+      min-height:200px;
+  }
+
+  /* Hide the carousel text when the screen is less than 600 pixels wide */
+  @media (max-width: 600px) {
+    .carousel-caption {
+      display: none; 
+    }
+  }
+  </style>
+</head>
+<body>
+
+<nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>                        
+      </button>
+      <a class="navbar-brand" href="#">CloudMeet</a>
+    </div>
+    <div class="collapse navbar-collapse" id="myNavbar">
+      
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="."><span class="glyphicon glyphicon-log-out"></span> Cerrar Sesión</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+ 
+
+   
+</div>
+  
+<div class="container text-center">    
+  <h2>Bienvenido a CloudMeet</h3><br>
+  <div class="row">
+    <div class="col-sm-12">
+      
+
+      <div class="list-group">
+        
+
+         
+          <h4 class="list-group-item-heading">Grupos a los que esta inscrito:</h4>
+     
+      
+
+
+        <a href="admin.php?idg=1" class="list-group-item ">
+          Academia de Ciencias
+        </a>
+
+        <a href="admin.php?idg=2" class="list-group-item ">
+          Academia de Programación
+        </a>
+
+              
+      </div>
+
+
+        
+       
+      
+    </div>
+     
+     
+  </div>
+</div><br>
+
+<footer class="container-fluid text-center">
+  <p>Footer Text</p>
+</footer>
+
+</body>
+</html>
